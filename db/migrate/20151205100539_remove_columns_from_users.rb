d35132d79d97ddf2c51dc1fc971ee25b7779b8ce
Simon Walsh <slfwalsh@gmail.com>
2015-12-05 11:14:41 +0000

    Added further questions

--- db/migrate/20151205100539_remove_columns_from_users.rb
+++
@@ -1,8 +0,0 @@
-class RemoveColumnsFromUsers < ActiveRecord::Migration
-  def change
-    remove_column :users, :type
-    remove_column :users, :solo
-    remove_column :users, :mdt
-    remove_column :users, :refer
-  end
-end
