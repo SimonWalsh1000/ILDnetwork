1a8829845d1b914181e32ad1b5bbc1df792b687b
Simon Walsh <slfwalsh@gmail.com>
2015-12-05 11:44:32 +0000

    Added further questions

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,5 @@ module Ers
     config.action_view.field_error_proc = Proc.new { |html_tag, instance|
       html_tag
     }
-
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151205100306) do
+ActiveRecord::Schema.define(version: 20151205113443) do
 
   create_table "members", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
