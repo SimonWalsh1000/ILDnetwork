371b191510a16d00f24d1a6a345226fa17aabfe6
Simon Walsh <slfwalsh@gmail.com>
2015-12-06 16:32:32 +0000

    Added validations

--- app/models/member.rb
+++ app/models/member.rb
@@ -21,7 +21,7 @@ class Member < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :trackable, :validatable
 
   has_one :user
 
