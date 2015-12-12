eb9025a27bd091d8e90b590750bfd6e95a3da633
Simon Walsh <slfwalsh@gmail.com>
2015-12-12 17:58:31 +0000

    mdt bit not working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -109,17 +109,14 @@ class UsersController < ApplicationController
                           .group_by{ |u| u.institute_type}
                           .map {|i,o| ["name" => i,  "y" => User.where(id: o.map(&:id)).sum("biopsy")/o.count, "n" => o.count]}
                           .flatten.to_json
-
-
-    @arr_phys = []
-    Physician.all.map { |u| u.user if u.user.country == @nation }.inject(Hash.new(0)) { |h, e| h[e] += 1 ; h }.select { |u, v| @arr_phys << v}
-    @arr_rads = []
-    Rad.all.map { |u| u.user if u.user.country == @nation }.inject(Hash.new(0)) { |h, e| h[e] += 1 ; h }.select { |u, v| @arr_rads << v}
-    @arr_paths = []
-    Path.all.map { |u| u.user if u.user.country == @nation }.inject(Hash.new(0)) { |h, e| h[e] += 1 ; h }.select { |u, v| @arr_paths << v}
-    @arr_rheum = []
-    Rheumatologist.all.select { |u| u.user.country == @nation}.inject(Hash.new(0)) { |h, e| h[e] += 1 ; h }.select { |u, v| @arr_rheum << v}
-
+    # @arr_phys = []
+    # Physician.all.map { |u| u.user if u.user.country == @nation }.inject(Hash.new(0)) { |h, e| h[e] += 1 ; h }.select { |u, v| @arr_phys << v}
+    # @arr_rads = []
+    # Rad.all.map { |u| u.user if u.user.country == @nation }.inject(Hash.new(0)) { |h, e| h[e] += 1 ; h }.select { |u, v| @arr_rads << v}
+    # @arr_paths = []
+    # Path.all.map { |u| u.user if u.user.country == @nation }.inject(Hash.new(0)) { |h, e| h[e] += 1 ; h }.select { |u, v| @arr_paths << v}
+    # @arr_rheum = []
+    # Rheumatologist.all.select { |u| u.user.country == @nation}.inject(Hash.new(0)) { |h, e| h[e] += 1 ; h }.select { |u, v| @arr_rheum << v}
     render 'country'
   end
 
