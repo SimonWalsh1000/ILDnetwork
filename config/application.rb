ae40f6fb0e0e482016cbd16198a63b34f7230be5
Simon Walsh <slfwalsh@gmail.com>
2015-12-23 19:19:43 +0000

    minor styling changes

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,5 @@ module Ers
     config.active_record.raise_in_transactional_callbacks = true
     config.action_view.field_error_proc = Proc.new {|html, instance| html}
     config.assets.initialize_on_precompile = false
-    config.serve_static_assets = true
   end
 end
